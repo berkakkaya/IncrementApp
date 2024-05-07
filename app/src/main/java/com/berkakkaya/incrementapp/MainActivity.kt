4ef84a849530bc89110f7aa571bb803565e28e83
Berk Akkaya <berkakkaya000@gmail.com>
2024-05-07 16:27:46 +0300

    initial commit

---
+++ app/src/main/java/com/berkakkaya/incrementapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.berkakkaya.incrementapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    var counter: Int = 0;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun onIncrementClicked(view: View) {
+        counter++
+
+        val textView: TextView = findViewById(R.id.textView)
+
+        textView.text = "Sayaç: $counter"
+    }
+}
